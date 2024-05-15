2f36e8894fa7cfd074f0ba4bb7cc97280dcfed40
migruiz <mig.ruiz@gmail.com>
2024-05-15 13:54:36 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,34 @@
-basic.forever(function () {
-	
-})
+namespace time {
+
+    //% block="pause $ms"
+    //% ms.shadow="timePicker"
+    export function pause(ms: number) {
+
+    }
+
+    /**
+     * Custom text dropdown
+     */
+
+    /**
+      * Get the word field editor
+      * @param word
+      */
+    //% blockId=wordPicker block="$word"
+    //% blockHidden=true
+    //% colorSecondary="#FFFFFF"
+    //% word.fieldEditor="textdropdown"
+    //% word.fieldOptions.decompileLiterals=true
+    //% word.fieldOptions.values='hi,hello'
+    //% word.defl='hello'
+    export function __wordPicker(word: string): string {
+        return word;
+    }
+
+
+    //% block="say $word"
+    //% word.shadow="wordPicker"
+    export function say(word: string) {
+
+    }
+}
