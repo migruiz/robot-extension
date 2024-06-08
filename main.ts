856ce40ae058f08a348ea4d6eb0b88ab732f1932
migruiz <mig.ruiz@gmail.com>
2024-06-08 05:58:36 +0100

    resetting matrix

--- main.ts
+++ main.ts
@@ -104,6 +104,23 @@ namespace robot {
             rotation_direction.clockwise,
             false
         )
+        basic.forever(function () {
+
+            basic.pause(2 * 60 * 1000)
+            max7219_matrix.setup(
+                4,
+                DigitalPin.P16,
+                DigitalPin.P15,
+                DigitalPin.P14,
+                DigitalPin.P13
+            )
+            max7219_matrix.for_4_in_1_modules(
+                rotation_direction.clockwise,
+                false
+            )
+        })
+
+
         kitroniKStrip = neopixel.create(DigitalPin.P1, 10, NeoPixelMode.RGB)
         leftEyeStrip = kitroniKStrip.range(0, 5)
         righEyeStrip = kitroniKStrip.range(5, 5)
@@ -127,12 +144,12 @@ namespace robot {
         clearText()
 
 
+        basic.pause(500)
         const serialData = {
             type: "Init"
         }
         const jsonData = JSON.stringify(serialData)
         serial.writeLine(jsonData)
-        basic.pause(300)
         Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo1)
         Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo2)
         Kitronik_Robotics_Board.servoStop(Kitronik_Robotics_Board.Servos.Servo5)
@@ -176,7 +193,7 @@ namespace robot {
         }
         const jsonData = JSON.stringify(serialData)
         serial.writeLine(jsonData)
-        basic.pause(500)
+        basic.pause(1000)
     }
 
     //% block
